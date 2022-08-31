1dcbedc3f6cbb27df362908ce1414c5408397053
Alexei Ionov <alexei.ionov@berkeley.edu>
2022-08-31 16:43:50 -0700

    lab02 submission

--- lab02/IntList/SquarePrimesTest.java
+++ lab02/IntList/SquarePrimesTest.java
@@ -17,4 +17,31 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void first_element_prime_test(){
+        IntList lst2 = IntList.of(7, 12, 13, 14, 15);
+        boolean changed = IntListExercises.squarePrimes(lst2);
+        assertEquals("49 -> 12 -> 169 -> 14 -> 15", lst2.toString());
+        assertTrue(changed);
+
+
+    }
+
+    @Test
+    public void no_primes(){
+        IntList lst3 = IntList.of(4, 4, 6, 8, 10, 100);
+        boolean changed = IntListExercises.squarePrimes(lst3);
+        assertEquals("4 -> 4 -> 6 -> 8 -> 10 -> 100", lst3.toString());
+        assertTrue(changed);
+
+    }
+    @Test
+    public void all_primes() {
+        IntList lst4 = IntList.of(1, 2, 3, 5, 7, 13);
+        boolean changed = IntListExercises.squarePrimes(lst4);
+        assertEquals("1 -> 4 -> 9 -> 25 -> 49 -> 169", lst4.toString());
+        assertTrue(changed);
+    }
+
+
 }
