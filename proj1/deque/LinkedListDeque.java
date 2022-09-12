72e56bf44221b11a35c1219e59eebb7aba670490
Alexei Ionov <alexei.ionov@berkeley.edu>
2022-09-11 22:05:51 -0700

    fixed style and API issues, i think

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,13 +1,11 @@
 package deque;
+public class LinkedListDeque<T> {
+    private class Node {
+        private T val;
+        private Node next;
+        private Node prev;
 
-
-public class LinkedListDeque<Item> {
-    private class Node{
-        public Item val;
-        public Node next;
-        public Node prev;
-
-        public Node(Item i, Node p, Node n){
+        private Node(T i, Node p, Node n) {
             val = i;
             next = n;
             prev = p;
@@ -17,176 +15,121 @@ public class LinkedListDeque<Item> {
     private int size;
 
     // constructor in the case that input is null; i.e, just create a lone sentinel node!
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         sentinel = new Node(null, null, null);
         size = 0;
-
     }
+
     // constructor in the case that input is not null!
-    public LinkedListDeque(Item x){
+    /**
+    public LinkedListDeque(T x) {
         sentinel = new Node(null, null, null);
         sentinel.next = new Node(x, sentinel, sentinel);
         sentinel.prev = sentinel.next;
         size = 1;
     }
+     */
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void add_to_empty_lst(Item val){
+    private void Add_To_Empty_List(T val) {
         sentinel.next = new Node(val, sentinel, sentinel);
         sentinel.prev = sentinel.next;
         size = 1;
-
     }
 
-    public void addFirst(Item val){
+    public void addFirst(T val) {
         if (isEmpty()) {
-            add_to_empty_lst(val);
+            Add_To_Empty_List(val);
         }
         else {
             Node temp = sentinel.next;
             sentinel.next = new Node(val, sentinel, temp);
             temp.prev = sentinel.next;
-            size +=1;
+            size += 1;
         }
-
     }
-    public void addLast(Item val){
-        if (isEmpty()){
-            add_to_empty_lst(val);
+    public void addLast(T val) {
+        if (isEmpty()) {
+            Add_To_Empty_List(val);
         }
-        else{
+        else {
             Node last = new Node(val, sentinel.prev, sentinel);
             sentinel.prev.next = last;
             sentinel.prev = last;
-            size +=1;
+            size += 1;
         }
-
     }
-    public boolean isEmpty(){
-        if (size ==0){
+    public boolean isEmpty() {
+        if (size == 0) {
             return true;
-
         }
         return false;
-
     }
-
-    public void printDeque(){
-        Node curr_node = sentinel.next;
-        while (curr_node != null){
-            System.out.print(curr_node.val + " ");
-            if (curr_node.next == sentinel){
+    public void printDeque() {
+        Node Curr_Node = sentinel.next;
+        while (Curr_Node != null) {
+            System.out.print(Curr_Node.val + " ");
+            if (Curr_Node.next == sentinel) {
                 break;
             }
 
-            curr_node = curr_node.next;
+            Curr_Node = Curr_Node.next;
         }
         System.out.println();
     }
-    public Item removeFirst(){
-        if (isEmpty()){
-
-            return null;
-        }
-        else{
-            Item res = sentinel.next.val;
-
-            sentinel.next = sentinel.next.next;
-            sentinel.next.prev = sentinel;
-            size -=1;
-
-            return res;
-
-        }
-
-    }
-    public Item removeLast() {
+    public T removeFirst() {
         if (isEmpty()) {
 
             return null;
         }
         else {
-            Item res = sentinel.prev.val;
+            T res = sentinel.next.val;
+            sentinel.next = sentinel.next.next;
+            sentinel.next.prev = sentinel;
+            size -= 1;
+            return res;
+        }
+    }
+    public T removeLast() {
+        if (isEmpty()) {
+
+            return null;
+        }
+        else {
+            T res = sentinel.prev.val;
             sentinel.prev = sentinel.prev.prev;
             sentinel.prev.next = sentinel;
             size -= 1;
             return res;
         }
     }
-    public Item get(int index){
-        int curr_index = 0;
+    public T get(int index) {
+        int Curr_Index = 0;
         Node head = sentinel.next;
-        while (head != null){
-            if (curr_index == index){
+        while (head != null) {
+            if (Curr_Index == index) {
                 return head.val;
             }
-            curr_index +=1;
+            Curr_Index += 1;
             head = head.next;
         }
         return null;
-
-
     }
-
-    /** public boolean compare_DLL(LinkedListDeque o, Node head){
-        Node compared_head = o.sentinel.next;
-        while((compared_head != null) && (head != null)){
-            if (compared_head.val != head.val){
-                return false;
-            }
-            compared_head = compared_head.next;
-            head = head.next;
-        }
-        if ((head == null && compared_head != null) || (compared_head == null && head != null)){
-            return false;
-        }
-        return true;
-    }
-
-     */
-
-
-    //NOT NEEDED FOR PROJ1a, comes later for part C!
-    /**
-    public boolean equals(Object o){
-        if (o instanceof LinkedListDeque){
-            return o.equals(sentinel.next);
-        }
-        return false;
-    }
-
-     */
-
-
-
-
-
-    public Item getRecursion_helper_fn(int index, Node head){
-        if (head == sentinel){
+    private T Get_Recursion_Helper_Fn(int index, Node head) {
+        if (head == sentinel) {
             return null;
         }
-        if (index == 0){
+        if (index == 0) {
             return head.val;
         }
-        return getRecursion_helper_fn(index -1, head.next);
+        return Get_Recursion_Helper_Fn(index - 1, head.next);
     }
 
-    public Item getRecursive(int index){
+    public T getRecursive(int index) {
         Node head = sentinel.next;
-        return getRecursion_helper_fn(index, head);
+        return Get_Recursion_Helper_Fn(index, head);
     }
-
-
-
-
-    public static void main(String[] args){
-        /// should call her input-ed LInked list deque. however, this means that temp if input is null is also null so will get eror
-
-
-    }
-
-
 }
