d7a8afc6e47781d5b6bce352cbaa4d666856aed4
Alexei Ionov <alexei.ionov@berkeley.edu>
2022-09-28 15:30:55 -0700

    redid program, now it uses a topsite and bottomsite

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,83 +3,68 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private int[][] matrix;
+    private boolean[][] matrix;
     private WeightedQuickUnionUF rootArr;
-    private boolean hasPercolated;
+    private int topSiteIndex;
+    private int botSiteIndex;
+    private int numOpen;
     public Percolation(int N) {
         /// in my matrix, 0s represent blocked cells, 1s represent open, 2s represent open + full
         if (N <= 0 ) {
             throw new IllegalArgumentException();
         }
-        matrix = new int[N][N];
-        rootArr = new WeightedQuickUnionUF(N*N);
-        hasPercolated = false;
+        // + 2 for top and bottom sites!
+        rootArr = new WeightedQuickUnionUF((N*N) + 2);
+        matrix = new boolean[N][N];
+        topSiteIndex = N*N;
+        botSiteIndex = N*N + 1;
+        numOpen = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = false;
+            }
+        }
     }
     private int matrixToUnionIndex(int row, int col) {
         return (row * matrix.length) + col;
     }
-
     public void open(int row, int col) {
-        if ((row < 0) || (row >= matrix.length) || (col < 0) || (col >= matrix.length)) {
-            throw new IndexOutOfBoundsException();
-        }
-        boolean openTop = false;
-        boolean openBot = false;
         if (!isOpen(row, col)) {
-
-            int rootArrIndex1 = matrixToUnionIndex(row, col);
+            matrix[row][col] = true;
+            numOpen += 1;
+            int rootIndex1 = matrixToUnionIndex(row, col);
             if (row == 0) {
-                matrix[row][col] = 2;
-                openTop = true;
-            } else if (row == matrix.length - 1) {
-
+                rootArr.union(rootIndex1, topSiteIndex);
             }
-            else {
-                matrix[row][col] = 1;
+            if (row == matrix.length - 1) {
+                rootArr.union(rootIndex1, botSiteIndex);
             }
-            // top row is [row-1, col]
-            if ((row - 1 >= 0) && (isOpen(row - 1, col))) {
-                int rootArrIndex2 = matrixToUnionIndex(row - 1, col); //parent!!
-                if (openTop) {
-                    int parentIndex = rootArr.find(rootArrIndex2);
-                    rootArr.parent[rootArrIndex1] = parentIndex;
-                } else {
-                    rootArr.union(rootArrIndex1, rootArrIndex2);
-                }
-
-            }
-            // top row is [row, col]
             if ((row + 1 < matrix.length) && (isOpen(row + 1, col))) {
-                int rootArrIndex2 = matrixToUnionIndex(row + 1, col);
-                if (openTop) {
-                    int parentIndex = rootArr.find(rootArrIndex2);
-                    rootArr[parentIndex] = rootArrIndex1;
-                } else {
-                    rootArr.union(rootArrIndex1, rootArrIndex2);
-                }
-            }
-
-            if ((col + 1 < matrix.length) && (isOpen(row, col + 1))) {
-                int rootArrIndex2 = matrixToUnionIndex(row, col + 1);
-                rootArr.union(rootArrIndex1, rootArrIndex2);
+                rootArr.union(rootIndex1, matrixToUnionIndex(row + 1,col));
             }
             if ((col - 1 >= 0) && (isOpen(row, col - 1))) {
-                int rootArrIndex2 = matrixToUnionIndex(row, col - 1);
-                rootArr.union(rootArrIndex1, rootArrIndex2);
+                rootArr.union(rootIndex1, matrixToUnionIndex(row,col - 1));
+            }
+            if ((row - 1 >= 0) && (isOpen(row-1, col))) {
+                rootArr.union(rootIndex1, matrixToUnionIndex(row-1,col));
+            }
+            if ((col + 1 < matrix.length) && (isOpen(row, col + 1))) {
+                rootArr.union(rootIndex1, matrixToUnionIndex(row,col + 1));
             }
         }
     }
     public boolean isOpen(int row, int col) {
-        return (matrix[row][col] != 0);
+        return matrix[row][col];
+    }
+    public boolean isFull(int row, int col) {
+        int index = matrixToUnionIndex(row, col);
+        return rootArr.connected(index, topSiteIndex);
+    }
+    public int numberOfOpenSites() {
+        return numOpen;
     }
     public boolean percolates() {
-        return hasPercolated;
+        return rootArr.connected(topSiteIndex, botSiteIndex);
     }
-
-
-
-
-
-
-
 }
