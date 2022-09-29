ddc41abb9391aaf5f5bcb42908f7b49be12e4d51
Alexei Ionov <alexei.ionov@berkeley.edu>
2022-09-28 21:10:54 -0700

    more small changes

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,7 @@
 package hw2;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
     private double[] percolationValues;
 
@@ -8,6 +10,9 @@ public class PercolationStats {
     private double std;
     private int numOfCells;
 
+    private Percolation newPercolation;
+
+
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException();
@@ -15,12 +20,11 @@ public class PercolationStats {
         numOfCells = N*N;
         percolationValues = new double[T];
         for (int i = 0; i < T; i++) {
-            Percolation newPercolation = pf.make(N);
+            newPercolation = pf.make(N);
             while (!newPercolation.percolates()) {
                 int row = StdRandom.uniform(N);
                 int col = StdRandom.uniform(N);
                 newPercolation.open(row, col);
-
             }
             percolationValues[i] = (double)(newPercolation.numberOfOpenSites() / numOfCells);
         }
@@ -30,20 +34,10 @@ public class PercolationStats {
     }
 
     public double mean() {
-        double sum = 0;
-        for (int i = 0; i < percolationValuesLength; i++) {
-            sum += percolationValues[i];
-        }
-        return (double)sum / percolationValuesLength;
+        return StdStats.mean(percolationValues);
     }
-
     public double stddev() {
-        double sum = 0;
-        for (int i = 0; i < percolationValuesLength; i++) {
-            sum += Math.pow((percolationValues[i] - meanVal), 2);
-        }
-        double sdSqared = (double)(sum / (percolationValuesLength - 1));
-        return Math.pow(sdSqared, 0.5);
+        return StdStats.stddev(percolationValues);
     }
     public double confidenceLow() {
         double sqT = Math.pow(percolationValuesLength, 0.5);
