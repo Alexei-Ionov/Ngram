3db2ccd9e322c04d9c982dfdef18e18cba7938a7
Alexei Ionov <alexei.ionov@berkeley.edu>
2022-10-12 15:45:42 -0700

    finished the plots, going to test

---
+++ proj2a/ngordnet/main/HistoryHandler.java
@@ -0,0 +1,34 @@
+package ngordnet.main;
+
+import ngordnet.hugbrowsermagic.NgordnetQuery;
+import ngordnet.hugbrowsermagic.NgordnetQueryHandler;
+import ngordnet.ngrams.NGramMap;
+import ngordnet.ngrams.TimeSeries;
+import ngordnet.plotting.Plotter;
+import org.knowm.xchart.XYChart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryHandler extends NgordnetQueryHandler {
+    private NGramMap ng;
+
+    public HistoryHandler(NGramMap map) {
+        ng = map;
+    }
+
+    @Override
+    public String handle(NgordnetQuery q) {
+        List<String> words = q.words();
+        List<TimeSeries> timeMaps = new ArrayList<>();
+        int index = 0;
+        for (String word: words) {
+            timeMaps.add(index, ng.weightHistory(word));
+            index += 1;
+        }
+        XYChart chart = Plotter.generateTimeSeriesChart(words, timeMaps);
+        String encodedImage = Plotter.encodeChartAsString(chart);
+        return encodedImage;
+    }
+
+}
